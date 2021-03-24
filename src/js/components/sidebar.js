64442aebb03de8903a768ad9ba4c0b047d707654
Kevin Oliveira <me@kevinoliveira.com.br>
2021-03-24 02:25:22 -0300

    Fix: (sidebar) Adicionando backdrop na sidebar

--- src/js/components/sidebar.js
+++ src/js/components/sidebar.js
@@ -1,8 +1,22 @@
 const sidebar = () => {
   const $el = document.querySelectorAll('[data-sidebar]');
+  const $body = document.querySelector('body');
   const buttons = Array.from($el);
   const $content = document.querySelector('.sidebar-content');
   const $list = document.querySelectorAll('[data-menu]');
+  const $backdrop = document.querySelector('.sidebar-backdrop');
+
+  $backdrop.addEventListener('click', (e) => {
+    $backdrop.classList.remove('active');
+    $body.classList.remove('overflow');
+    $content.classList.remove('active');
+    buttons.forEach((btn) => {
+      btn.classList.remove('active');
+    });
+    $list.forEach((list) => {
+      list.style.display = 'none';
+    });
+  });
 
   buttons.forEach((btn) => {
     btn.addEventListener('click', (e) => {
@@ -20,6 +34,8 @@ const sidebar = () => {
       $selected.style.display = 'block';
       $btn.classList.add('active');
       $content.classList.remove('active');
+      $body.classList.toggle('overflow');
+      $backdrop.classList.add('active');
 
       setTimeout(() => {
         $content.classList.add('active');
