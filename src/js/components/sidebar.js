d88aeb2bbe219170efe492aeb0a5bfdc22c5af4e
Kevin Oliveira <me@kevinoliveira.com.br>
2021-03-24 01:07:35 -0300

    feat(sidebar): Implementação das listas na sidebar

--- src/js/components/sidebar.js
+++ src/js/components/sidebar.js
@@ -2,16 +2,29 @@ const sidebar = () => {
   const $el = document.querySelectorAll('[data-sidebar]');
   const buttons = Array.from($el);
   const $content = document.querySelector('.sidebar-content');
+  const $list = document.querySelectorAll('[data-menu]');
 
   buttons.forEach((btn) => {
     btn.addEventListener('click', (e) => {
       const $btn = e.target;
+      const $selected = document.querySelector(
+        `[data-menu=${$btn.dataset.sidebar}]`
+      );
       buttons.forEach((btn) => {
         btn.classList.remove('active');
       });
+      $list.forEach((list) => {
+        list.style.display = 'none';
+      });
+
+      $selected.style.display = 'block';
 
       $btn.classList.add('active');
-      $content.classList.add('active');
+      $content.classList.remove('active');
+
+      setTimeout(() => {
+        $content.classList.add('active');
+      }, 300);
     });
   });
 };
