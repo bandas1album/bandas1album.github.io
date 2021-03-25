62dc549edd92edc4c2ece7a631bb4b042c15313c
Kevin Oliveira <me@kevinoliveira.com.br>
2021-03-25 02:23:17 -0300

    Fix: Ajuste na scrollbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import toggle from './components/toggle';
 import sidebar from './components/sidebar';
+// import scrollbar from './components/scrollbar';
 
 toggle('[data-open]');
 sidebar();
+// scrollbar();
